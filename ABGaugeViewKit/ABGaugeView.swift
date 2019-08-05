8a303f87c6be2f78bd4d24a86cf35fa95dc6d742
Tobias Tiemerding <tobias@tiemerding.com>
2019-08-05 22:06:39 +0200

    Update ABGaugeView.swift

--- ABGaugeViewKit/ABGaugeView.swift
+++ ABGaugeViewKit/ABGaugeView.swift
@@ -16,7 +16,10 @@ public class ABGaugeView: UIView {
     @IBInspectable public var colorCodes: String = "929918,C8CC86,66581A,3A4A73,185D99"
     @IBInspectable public var startAngles: String = "0,20,40,60,80"
     @IBInspectable public var arcAngle: CGFloat = 260
-    @IBInspectable public var needleColor: UIColor = UIColor(red: 18/255.0, green: 112/255.0, blue: 178/255.0, alpha: 1.0)
+    
+    @IBInspectable public var needleColor: UIColor
+    @IBInspectable public var circleColor: UIColor
+
     @IBInspectable public var needleValue: CGFloat = 0 {
         didSet {
             setNeedsDisplay()
@@ -27,10 +30,13 @@ public class ABGaugeView: UIView {
             capStyle = isRoundCap ? .round : .butt
         }
     }
-    @IBInspectable public var circleColor: UIColor = UIColor.black
+   
     
     var currentRadians: CGFloat? = nil
-    var capStyle = CGLineCap.round
+    var capStyle: CGLineCap = .round
+    
+    var circleLayer: CAShapeLayer? = nil
+    var needleLayer: CAShapeLayer? = nil
     
     
     // MARK:- UIView Draw method
@@ -39,11 +45,27 @@ public class ABGaugeView: UIView {
     }
     
     public override init(frame: CGRect) {
+        if #available(iOS 13.0, *) {
+            needleColor = .secondaryLabel
+            circleColor = .label
+        } else {
+            needleColor = .black
+            circleColor = .black
+        }
+        
         super.init(frame: frame)
         drawGauge()
     }
     
     required public init?(coder aDecoder: NSCoder) {
+        if #available(iOS 13.0, *) {
+            needleColor = .secondaryLabel
+            circleColor = .label
+        } else {
+            needleColor = .black
+            circleColor = .black
+        }
+        
         super.init(coder: aDecoder)
         drawGauge()
     }
@@ -55,7 +77,6 @@ public class ABGaugeView: UIView {
     
     // MARK:- Custom Methods
     func drawGauge() {
-        layer.sublayers = []
         drawSmartArc()
         drawNeedle()
         drawNeedleCircle()
@@ -79,8 +100,8 @@ public class ABGaugeView: UIView {
         }
     }
     
-    func radian(for percentOfArc: CGFloat) -> CGFloat {
-        let startAngle:CGFloat = 90.0 + abs(arcAngle-360.0) / 2.0
+    func radian(for percentOfArc: CGFloat, offset: CGFloat = 0.0) -> CGFloat {
+        let startAngle:CGFloat = offset + abs(arcAngle-360.0) / 2.0
         let degrees = arcAngle * (percentOfArc / 100.0)
         return (startAngle + degrees) * .pi/180
     }
@@ -89,24 +110,19 @@ public class ABGaugeView: UIView {
         var angles = [CGFloat]()
         for angle in startAngles.components(separatedBy: ",") {
             guard let number = NumberFormatter().number(from: angle) else { continue }
-            angles.append(radian(for: CGFloat(truncating: number)))
+            angles.append(radian(for: CGFloat(truncating: number), offset: 90.0))
         }
-        angles.append(radian(for: 100))
+        angles.append(radian(for: 100, offset: 90.0))
         return angles
     }
     
     func createArcWith(startAngle: CGFloat, endAngle: CGFloat, arcCap: CGLineCap, strokeColor: UIColor, center:CGPoint) {
-        // 1
         let center = center
         let radius: CGFloat = max(bounds.width, bounds.height)/2 - frame.width/20
         let lineWidth: CGFloat = frame.width/10
-        // 2
-        let path = UIBezierPath(arcCenter: center,
-                                radius: radius,
-                                startAngle: startAngle,
-                                endAngle: endAngle,
-                                clockwise: true)
-        // 3
+        
+        let path = UIBezierPath(arcCenter: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: true)
+        
         path.lineWidth = lineWidth
         path.lineCapStyle = arcCap
         strokeColor.setStroke()
@@ -114,51 +130,47 @@ public class ABGaugeView: UIView {
     }
     
     func drawNeedleCircle() {
-        // 1
-        let circleLayer = CAShapeLayer()
-        let circlePath = UIBezierPath(arcCenter: CGPoint(x: bounds.width / 2, y: bounds.height / 2), radius: bounds.width/20, startAngle: 0.0, endAngle: CGFloat(2 * Double.pi), clockwise: false)
-        // 2
-        circleLayer.path = circlePath.cgPath
-        circleLayer.fillColor = circleColor.cgColor
-        layer.addSublayer(circleLayer)
+        if circleLayer == nil {
+            let circlePath = UIBezierPath(arcCenter: CGPoint(x: bounds.width / 2, y: bounds.height / 2), radius: bounds.width/20, startAngle: 0.0, endAngle: CGFloat(2 * Double.pi), clockwise: false)
+            
+            circleLayer = CAShapeLayer()
+            circleLayer!.path = circlePath.cgPath
+            
+            layer.addSublayer(circleLayer!)
+        }
+        circleLayer!.fillColor = circleColor.cgColor
     }
     
     func drawNeedle() {
-        let needlePath = UIBezierPath()
-        needlePath.move(to: CGPoint(x: bounds.width/2, y: bounds.width * 0.95))
-        needlePath.addLine(to: CGPoint(x: bounds.width * 0.47, y: bounds.width * 0.42))
-        needlePath.addLine(to: CGPoint(x: bounds.width * 0.53, y: bounds.width * 0.42))
-        
-        needlePath.close()
-        
-        let triangleLayer = CAShapeLayer()
-        triangleLayer.frame = bounds
-        triangleLayer.path = needlePath.cgPath
-        
-        triangleLayer.fillColor = needleColor.cgColor
-        triangleLayer.strokeColor = needleColor.cgColor
+        if needleLayer == nil {
+            let needlePath = UIBezierPath()
+            needlePath.move(to: CGPoint(x: bounds.width/2, y: bounds.width * 0.95))
+            needlePath.addLine(to: CGPoint(x: bounds.width * 0.47, y: bounds.width * 0.42))
+            needlePath.addLine(to: CGPoint(x: bounds.width * 0.53, y: bounds.width * 0.42))
+            needlePath.close()
 
-        layer.addSublayer(triangleLayer)
-        
-        let percentOfArc:CGFloat = needleValue
-        let startAngle:CGFloat = abs(arcAngle-360.0) / 2.0
-        let degrees = arcAngle * (percentOfArc / 100.0)
-        let radians = (startAngle + degrees) * .pi/180
+            needleLayer = CAShapeLayer()
+            needleLayer!.frame = bounds
+            needleLayer!.path = needlePath.cgPath
+
+            layer.addSublayer(needleLayer!)
+        }
+        needleLayer!.fillColor = needleColor.cgColor
+        needleLayer!.strokeColor = needleColor.cgColor
         
         if currentRadians == nil {
+            let startAngle:CGFloat = abs(arcAngle-360.0) / 2.0
             currentRadians = startAngle * .pi/180
         }
-        if currentRadians! == radians {
-            return
-        }
+        let radians = radian(for: needleValue)
         
-        animate(triangleLayer: triangleLayer, fromValue: currentRadians!, toValue: radians, duration: 0.5, callBack: {
+        let timeInterval: CFTimeInterval = (currentRadians! != radians) ? 0.6 : 0
+        animate(triangleLayer: needleLayer!, fromValue: currentRadians!, toValue: radians, duration: timeInterval, callBack: {
             self.currentRadians = radians
         })
     }
     
     func animate(triangleLayer: CAShapeLayer, fromValue: CGFloat, toValue:CGFloat, duration: CFTimeInterval, callBack:@escaping ()->Void) {
-        // 1
         CATransaction.begin()
         let spinAnimation1 = CABasicAnimation(keyPath: "transform.rotation.z")
         spinAnimation1.fromValue = fromValue
@@ -170,7 +182,7 @@ public class ABGaugeView: UIView {
         CATransaction.setCompletionBlock {
             callBack()
         }
-        // 2
+        
         triangleLayer.add(spinAnimation1, forKey: "indeterminateAnimation")
         CATransaction.commit()
     }
