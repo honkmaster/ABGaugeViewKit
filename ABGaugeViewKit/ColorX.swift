922ae39a282666194dd6b91b1ad5bd53831d2838
Tobias Tiemerding <tobias@tiemerding.com>
2019-08-05 22:07:04 +0200

    Update ColorX.swift

--- ABGaugeViewKit/ColorX.swift
+++ ABGaugeViewKit/ColorX.swift
@@ -16,36 +16,17 @@ extension UIColor {
     }
     
     convenience init(hex: String, alpha: CGFloat) {
-        var hexWithoutSymbol = hex
-        if hexWithoutSymbol.hasPrefix("#") {
-            hexWithoutSymbol = String(hexWithoutSymbol.dropFirst())
-        }
+        let scanner = Scanner(string: hex)
+        scanner.charactersToBeSkipped = CharacterSet(charactersIn: "#")
         
-        let scanner = Scanner(string: hexWithoutSymbol)
         var hexInt:UInt32 = 0x0
         scanner.scanHexInt32(&hexInt)
+
+        let red = CGFloat((hexInt & 0xff0000) >> 16) / 255.0
+        let green = CGFloat((hexInt & 0xff00) >> 8) / 255.0
+        let blue = CGFloat((hexInt & 0xff) >> 0) / 255.0
         
-        var r:UInt32!, g:UInt32!, b:UInt32!
-        switch (hexWithoutSymbol.count) {
-        case 3: // #RGB
-            r = ((hexInt >> 4) & 0xf0 | (hexInt >> 8) & 0x0f)
-            g = ((hexInt >> 0) & 0xf0 | (hexInt >> 4) & 0x0f)
-            b = ((hexInt << 4) & 0xf0 | hexInt & 0x0f)
-            break;
-        case 6: // #RRGGBB
-            r = (hexInt >> 16) & 0xff
-            g = (hexInt >> 8) & 0xff
-            b = hexInt & 0xff
-            break;
-        default:
-            // TODO:ERROR
-            break;
-        }
         
-        self.init(
-            red: (CGFloat(r)/255),
-            green: (CGFloat(g)/255),
-            blue: (CGFloat(b)/255),
-            alpha:alpha)
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
     }
 }
